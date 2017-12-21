ffb5048dc926af2a48a2b1d59a6a49df53b0b713
zhangkenan <zhangkenan@houbank.cn>
2017-12-21 17:42:27 +0800

    rabbitmq学习实例

---
+++ README.md
@@ -0,0 +1,39 @@
+#rabbitMQ repository
+主要记录个人学习reabbit的相关demo
+
+## rabbitmq-java-helloworld
+只是第一个rabbitmq的例子 => hello world
+消费者不能关闭连接，需要一直轮询查看是否存在消息
+
+## rabbitmq-java-queque
+rabbitmq任务队列示例
+
+## rabbitmq-java-publish-receive
+rabbitmq发布订阅示例
+
+## rabbitmq-java-routing
+rabbitmq路由示例
+
+## rabbitmq-spring-topic-exchange
+rabbitmq主题示例
+
+## rabbitmq-java-rpc
+rabbitmq PRC通信示例
+
+## rabbitmq-spring-helloworld
+spring boot 使用rabbitmq的第一个demo
+
+## rabbitmq-spring-work-queue
+spring boot使用rabbitmq的队列示例
+
+## rabbitmq-spring-fanout
+spring boot使用的rabbitmq的发布订阅示例
+
+## rabbitmq-spring-direct-exchange
+spring boot 使用rabbitmq的路由示例
+
+## rabbitmq-spring-topic-exchange
+spring boot使用rabbitmq的主题示例
+
+## rabbitmq-spring-rpc
+sprng boot使用rabbitmq的rpc通信示例
---
+++ rabbitmq-spring-helloworld/src/main/java/com/eumji/rabbitmq/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,29 @@
+package com.eumji.rabbitmq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 模拟初始化过程
+ * @email eumji025@gmail.com
+ * @author:EumJi
+ * @date: 2017/9/4
+ * @time: 15:26
+ */
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        System.out.println("Ready ... running for " + duration + "ms");
+        Thread.sleep(duration);
+        ctx.close();
+    }
+}
